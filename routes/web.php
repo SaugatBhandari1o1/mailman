823b33799a4e4e287b0fc22cc64254423331f4f5
SaugatBhandari1o1 <bhandarisaugat56@gmail.com>
2024-02-20 15:34:58 +0545

    Update: Breeze login and email verification fixed

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,27 +11,58 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider and will be assigned to
+| the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {return view('welcome');});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware(['auth', 'verified'])->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
+// Route::middleware(['auth', 'verified'])->group(function () {
+//     Route::get('/home', function () {
+//         return view('home');
+//     })->name('home');
+
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+   // Route::middleware(['auth', 'verified'])->group(function () {
+    //     Route::get('/home', function () {
+    //         return view('home');
+    //     })->name('home');
+
+ 
+    Route::get('/home', function () {
+        return view('home');
+    })->name('home');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+
+
 
 Route::middleware('guest')->group(function () {
-Route::get('/', function () {return view('/auth/login');}); 
+    Route::get('/', function () {
+        return view('/auth/login');
+    });
 });
 
-
-
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
